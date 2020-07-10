dd3e3ecedf5aa09be4dfaf13d948270db557d502
Philipp Fritsche <ph.fritsche@gmail.com>
2020-07-10 11:29:28 +0200

    Add tests

---
+++ test/properties.js
@@ -0,0 +1,35 @@
+import { sortPropertyKeys, sortProperties, mapProperties } from '../src/properties'
+
+describe('Property helpers', () => {
+    it('Sort keys', () => {
+        const object = {
+            a: {propertyOrder: 2},
+            b: {propertyOrder: 1},
+            c: {propertyOrder: 1},
+        }
+
+        expect(sortPropertyKeys(object)).toEqual(['b','c','a'])
+    })
+
+    it('Sort properties', () => {
+        const object = {
+            a: {propertyOrder: 2},
+            b: {propertyOrder: 1},
+            c: {propertyOrder: 1},
+        }
+
+        const sortedObject = sortProperties(object)
+        expect(Object.keys(sortedObject)).toEqual(['b','c','a'])
+        expect(sortedObject).toEqual(object)
+    })
+
+    it('Map properties', () => {
+        const object = {
+            a: {v: 'foo', propertyOrder: 2},
+            b: {v: 'bar', propertyOrder: 1},
+            c: {v: 'baz', propertyOrder: 1},
+        }
+
+        expect(mapProperties(object, (a,i) => i + ':' + a.v)).toEqual(['b:bar','c:baz','a:foo'])
+    })
+})
