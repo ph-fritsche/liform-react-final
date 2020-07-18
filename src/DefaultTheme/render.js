51102a3b1dd56ae096fb87a8621b085263a7168a
Philipp Fritsche <ph.fritsche@gmail.com>
2020-07-18 17:28:12 +0200

    Handle optional schema prop

--- src/DefaultTheme/render.js
+++ src/DefaultTheme/render.js
@@ -4,8 +4,8 @@ export const Container = (props) => (
     <form
         onSubmit={props.handleSubmit}
         onReset={props.handleReset}
-        method={props.method || props.liform.schema.method || 'POST'}
-        action={props.action || props.liform.schema.action}
+        method={props.method || props.liform.schema && props.liform.schema.method || 'POST'}
+        action={props.action || props.liform.schema && props.liform.schema.action || ''}
     >
         { props.children }
     </form>
