33df4387ca39d51f5c959dc0bcd71d9f17baa060
Philipp Fritsche <ph.fritsche@gmail.com>
2020-07-19 11:46:47 +0200

    Add dev-server config

---
+++ liform.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Liform, { DefaultTheme } from './src'
+
+Liform.theme = DefaultTheme
+
+import props from './props.json'
+
+ReactDOM.render(
+  <Liform {...props} name='form'>
+    <footer>{(renderProps) => (
+      <pre>
+        <code>
+          {JSON.stringify(renderProps.values, null, 2)}
+        </code>
+      </pre>
+    )}</footer>
+  </Liform>,
+  document.getElementById('liform')
+)
