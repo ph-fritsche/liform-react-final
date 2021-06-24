30e90bda5486da476f6ee91da6b0d1ec1627e6fb
Philipp Fritsche <ph.fritsche@gmail.com>
2021-06-24 12:31:59 +0000

    chore: replace webpack with esbuild

--- public/liform.jsx
+++ public/liform.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Liform, { DefaultTheme } from './src'
+import Liform, { DefaultTheme } from '../src'
 
 Liform.theme = DefaultTheme
 
